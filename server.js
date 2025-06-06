ba6476498933ea3206c87ab558cdc4b3f3b171a3
KimJunWoo <kjw4523705@naver.com>
2025-06-07 02:10:33 +0900

    :sparkles: feat: PORT변경

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const app = require('./app');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
